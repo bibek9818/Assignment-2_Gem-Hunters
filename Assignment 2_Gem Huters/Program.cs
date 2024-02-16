8ca9c6c4a84baa4d6bb9036118f00349dcfe12e6
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-02-15 20:15:41 -0500

    Add project files.

---
+++ Assignment 2_Gem Huters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+
+
+
+public void PlaceGemsAndObstacles()
+{
+
+
+
+}
+
+
+
+
+//Creating the display method
+public void Display()
+{
+
+}
+
+
+//Creating the method for collectinggem
+public void CollectGem()
+{
+
+}
+
+
