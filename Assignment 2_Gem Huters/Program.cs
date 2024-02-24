ef232a37b9d1d6a04e9ed62f092901675d27544d
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-02-23 23:28:31 -0500

    Created new class and methods inside them

--- Assignment 2_Gem Huters/Program.cs
+++ Assignment 2_Gem Huters/Program.cs
@@ -1,5 +1,88 @@
 ﻿using System;
+using System.Xml.Linq;
 
+
+
+
+
+class Player
+{
+
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+
+
+
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                if (Position.Y > 0)
+                {
+                    Position.Y--;
+                }
+                break;
+            case 'D':
+                if (Position.Y < 5)
+                {
+                    Position.Y++;
+                }
+                break;
+            case 'L':
+                if (Position.X > 0)
+                {
+                    Position.X--;
+                }
+                break;
+            case 'R':
+                if (Position.X < 5)
+                {
+                    Position.X++;
+                }
+                break;
+        }
+    }
+
+
+
+
+
+}
+
+
+
+class game
+{
+    public void Start()
+    {
+        do
+        {
+            Console.Clear();
+            Board.Display();
+            Console.WriteLine($"{CurrentTurn.Name}'s turn. Enter move (U/D/L/R): ");
+            char move = Console.ReadKey().KeyChar;
+
+            if (Board.IsValidMove(CurrentTurn, move))
+            {
+                CurrentTurn.Move(move);
+                Board.CollectGem(CurrentTurn);
+                TotalTurns++;
+                SwitchTurn();
+            }
+
+        } while (!IsGameOver());
+
+        Console.Clear();
+        Board.Display();
+        AnnounceWinner();
+    }
+}
 
 class Position
 {
